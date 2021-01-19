7688bab4ad6df69daa68837fc27e5aac96704aa6
naojulius <naojulius.mg@gmail.com>
2021-01-19 10:35:36 +0300

    fist deploy

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 5000;
 
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/public'));
 // });
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
+app.listen(port, () => console.log(`Listen on port ${port}!`));
